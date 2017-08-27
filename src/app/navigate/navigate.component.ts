59dcfe54cf09665a946dd88c170f3255f2dfa13f
jcdelgadoramos <jcdelgadoramos@outlook.com>
2017-08-26 20:32:43 -0500

    Segundo avance, correccion apariencia inicial

--- src/app/navigate/navigate.component.ts
+++ src/app/navigate/navigate.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigate.component.css']
 })
 export class NavigateComponent implements OnInit {
+  items = [
+    { titulo: 'The haunting happening', autor: 'Kenneth Cunningham', numero_ejemplares: 3 },
+    { titulo: 'Lovely Lovelace', autor: 'Leopolda Coimbra', numero_ejemplares: 5 },
+    { titulo: 'La Biblia Morada', autor: 'Ginger Gainsburg', numero_ejemplares: 1 },
+    { titulo: 'Apuntes sobre la desgracia del pueblo', autor: 'Leopolda Coimbra', numero_ejemplares: 2 },
+    { titulo: 'El toro', autor: 'Parker Graham', numero_ejemplares: 53 }
+  ]; 
 
   constructor() { }
 
   ngOnInit() {
+  }
+  
+  getMaterials() {
+    return this.items.slice();
   }
 
 }
